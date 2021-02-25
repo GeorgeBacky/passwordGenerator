2baf2e3c0f9f8a244d03cf2d2f45e3077075cb4a
George Bakalis <georgebak2182@gmail.com>
2021-02-25 18:49:29 +0200

    Add project files.

---
+++ passwordGenerator/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace passwordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.xuiGradientPanel1 = new XanderUI.XUIGradientPanel();
+            this.textResults = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.credits = new System.Windows.Forms.Label();
+            this.xuiSuperButton1 = new XanderUI.XUISuperButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.xuiGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // xuiGradientPanel1
+            // 
+            this.xuiGradientPanel1.BottomLeft = System.Drawing.Color.Black;
+            this.xuiGradientPanel1.BottomRight = System.Drawing.Color.DarkSlateBlue;
+            this.xuiGradientPanel1.Controls.Add(this.textResults);
+            this.xuiGradientPanel1.Controls.Add(this.button1);
+            this.xuiGradientPanel1.Controls.Add(this.credits);
+            this.xuiGradientPanel1.Controls.Add(this.xuiSuperButton1);
+            this.xuiGradientPanel1.Controls.Add(this.label2);
+            this.xuiGradientPanel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.xuiGradientPanel1.Location = new System.Drawing.Point(-1, -2);
+            this.xuiGradientPanel1.Name = "xuiGradientPanel1";
+            this.xuiGradientPanel1.PrimerColor = System.Drawing.Color.White;
+            this.xuiGradientPanel1.Size = new System.Drawing.Size(684, 399);
+            this.xuiGradientPanel1.Style = XanderUI.XUIGradientPanel.GradientStyle.Corners;
+            this.xuiGradientPanel1.TabIndex = 0;
+            this.xuiGradientPanel1.TopLeft = System.Drawing.Color.DarkTurquoise;
+            this.xuiGradientPanel1.TopRight = System.Drawing.Color.SlateBlue;
+            // 
+            // textResults
+            // 
+            this.textResults.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(105)))), ((int)(((byte)(147)))));
+            this.textResults.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResults.ForeColor = System.Drawing.Color.White;
+            this.textResults.Location = new System.Drawing.Point(250, 138);
+            this.textResults.Name = "textResults";
+            this.textResults.Size = new System.Drawing.Size(197, 33);
+            this.textResults.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(637, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 34);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // credits
+            // 
+            this.credits.AutoSize = true;
+            this.credits.BackColor = System.Drawing.Color.Transparent;
+            this.credits.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.credits.ForeColor = System.Drawing.SystemColors.Control;
+            this.credits.Location = new System.Drawing.Point(468, 363);
+            this.credits.Name = "credits";
+            this.credits.Size = new System.Drawing.Size(163, 17);
+            this.credits.TabIndex = 3;
+            this.credits.Text = "Created by George Bakalis";
+            // 
+            // xuiSuperButton1
+            // 
+            this.xuiSuperButton1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.xuiSuperButton1.ButtonImage = null;
+            this.xuiSuperButton1.ButtonSmoothing = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            this.xuiSuperButton1.ButtonStyle = XanderUI.XUISuperButton.Style.RoundedEdges;
+            this.xuiSuperButton1.ButtonText = "Generate";
+            this.xuiSuperButton1.CornerRadius = 5;
+            this.xuiSuperButton1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xuiSuperButton1.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton1.HoverBackgroundColor = System.Drawing.Color.MediumPurple;
+            this.xuiSuperButton1.HoverTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.ImagePosition = XanderUI.XUISuperButton.imgPosition.Left;
+            this.xuiSuperButton1.Location = new System.Drawing.Point(250, 218);
+            this.xuiSuperButton1.Name = "xuiSuperButton1";
+            this.xuiSuperButton1.SelectedBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.xuiSuperButton1.SelectedTextColor = System.Drawing.Color.White;
+            this.xuiSuperButton1.Size = new System.Drawing.Size(197, 40);
+            this.xuiSuperButton1.SuperSelected = false;
+            this.xuiSuperButton1.TabIndex = 2;
+            this.xuiSuperButton1.TextColor = System.Drawing.Color.Black;
+            this.xuiSuperButton1.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiSuperButton1.Click += new System.EventHandler(this.xuiSuperButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(176, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password Generator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 395);
+            this.Controls.Add(this.xuiGradientPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(698, 434);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(698, 434);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password Generator";
+            this.xuiGradientPanel1.ResumeLayout(false);
+            this.xuiGradientPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private XanderUI.XUIGradientPanel xuiGradientPanel1;
+        private XanderUI.XUISuperButton xuiSuperButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label credits;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textResults;
+    }
+}
+
---
+++ passwordGenerator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void xuiSuperButton1_Click(object sender, EventArgs e)
+        {
+            textResults.Text = string.Empty;
+            int length = 10;
+            // Create a string of characters, numbers, special characters that allowed in the password  
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+                textResults.Text += chars[i];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.linkedin.com/in/giwrgos-bakalis01/");
+        }
+    }
+}
